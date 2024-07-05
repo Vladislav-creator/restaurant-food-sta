c82c8630d7b27eeb8ad70600e579fbceb2730e63
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-07-05 11:50:47 +0300

    1

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,38 +1,4 @@
 'use client'
-// import { usePathname } from 'next/navigation';
-// import { useSelector } from 'react-redux';
-// import Link from "next/link";
-// import Image from 'next/image';
-//  import {selectCartTotal} from '../Redux/ordersSlice';
-// // import styles from './Header.module.css';
-// const Header = () => {
-//   const totalAmount = useSelector(selectCartTotal);
-//   const totalAmountHeader = totalAmount ? `: ${totalAmount} грн.` : '';
-  
-//   const pathname = usePathname() 
-//   return (
-//     <header>
-//        <Image
-//            src="./img/hero/food-sta.svg"
-//            alt="food-sta"
-//           //  className={styles.foodSta}
-//            width={66}
-//            height={52}
-//            priority
-//          />
-//         <nav>
-//         <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">Головна</Link>
-//         <Link className={`link ${pathname === '/about' ? 'active' : ''}`} href="/about">Про нас</Link>
-//         <Link className={`link ${pathname === '/contacts' ? 'active' : ''}`} href="/contacts">Контакти</Link>
-//         <Link className={`myOrder ${pathname === '/my-order' ? 'active' : ''}`} href="/my-order">Мій заказ{totalAmountHeader} </Link>
-//         </nav>
-//     </header>
-//   )
-// }
-
-// export default Header
-
-
 import { useState } from 'react';
 import Modal from '../Modal/Modal'; // Ваш компонент модального окна
 import OrderSummary from '../OrderSummary/OrderSummary';
@@ -60,7 +26,7 @@ const Header = () => {
   return (
     <header>
       <Image
-        src="/img/hero/food-sta.svg"
+        src="./img/hero/food-sta.svg"
         alt="food-sta"
         width={66}
         height={52}
