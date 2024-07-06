f76897cb8e09471fff0323a39dca50c656392f18
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-07-06 17:36:49 +0300

    1

--- app/services.js
+++ app/services.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 export const getAllDishes = async () => {
   const { data } = await axios.get(
-     `https://backend-restaurant-food-sta.onrender.com/dishes`
-      //`http://localhost:8000/dishes`
+      `https://backend-restaurant-food-sta.onrender.com/dishes`
+     // `http://localhost:8000/dishes`
   );
 
   return data;
 };
 export const getCooks = async () => {
   const { data } = await axios.get(
-      `https://backend-restaurant-food-sta.onrender.com/dishes`
+      `https://backend-restaurant-food-sta.onrender.com/cooks`
      // `http://localhost:8000/cooks`
   );
 
