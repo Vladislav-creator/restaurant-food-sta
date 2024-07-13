1ca6876d494144efc7ac5a51254439b4867c896e
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-07-13 20:36:27 +0300

    555

--- app/about/page.js
+++ app/about/page.js
@@ -1,33 +1,4 @@
 
-// import AboutComponent from '../components/AboutComponent/AboutComponent';
-// import style from './about.module.css';
-
-// export const metadata = {
-//   title: 'Команда поваров Кафе-ресторана "Food Sta"',
-//   description: 'Про команду поваров Кафе-ресторана "Food Sta"',
-// };
-
-// async function fetchCooksData() {
-//   const { getCooks } = await import('../services');
-//   const data = await getCooks();
-//   const headChef = data.find(cook => cook.position === "Головний кухар");
-//   const otherCooks = data.filter(cook => cook.position !== "Головний кухар");
-//   return { headChef, otherCooks };
-// }
-
-// const AboutPage = async () => {
-//   const { headChef, otherCooks } = await fetchCooksData();
-
-//   return (
-//     <div className={style.colorText}>
-//       <AboutComponent headChef={headChef} otherCooks={otherCooks} />
-//     </div>
-//   );
-// };
-
-// export default AboutPage;
-
-// app/about/page.js
 import AboutComponent from '../components/AboutComponent/AboutComponent';
 import style from './about.module.css';
 
@@ -55,3 +26,5 @@ const AboutPage = async () => {
 };
 
 export default AboutPage;
+
+
