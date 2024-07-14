7c4deca7f224ed20bf0611f981b6a720043f5296
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-07-14 12:03:35 +0300

    222

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,53 @@
+// "use client";
+
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import store, { persistor } from './components/Redux/store'; // пути могут отличаться
+// import { Inter } from "next/font/google";
+//  import Header from './components/Header/Header';
+// import "./globals.css";
+// import 'leaflet/dist/leaflet.css';
+// const inter = Inter({ subsets: ["latin"] });
+
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+     
+//       <body className={inter.className}>
+//         <Provider store={store}>
+//           <PersistGate loading={null} persistor={persistor}>
+//             <Header />
+//             <main className="container">
+//               {children}
+//             </main>
+//           </PersistGate>
+//         </Provider>
+//       </body>
+//     </html>
+//   );
+// }
 
 "use client";
-
+import localFont from 'next/font/local'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './components/Redux/store'; // пути могут отличаться
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
  import Header from './components/Header/Header';
 import "./globals.css";
 import 'leaflet/dist/leaflet.css';
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
-
+const myFont = localFont({
+  src: '../public/ofontMiama-Nueva.ttf',
+  display: 'swap',
+})
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
      
-      <body className={inter.className}>
+      <body className={myFont.className}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <Header />
